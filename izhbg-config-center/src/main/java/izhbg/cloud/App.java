a00c1af250d97efda1f26740e4702ec1e27d76de
464470532@qq.com <1987114>
2018-08-03 11:06:28 +0800

    Initial commit

---
+++ izhbg-config-center/src/main/java/izhbg/cloud/App.java
@@ -0,0 +1,13 @@
+package izhbg.cloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
